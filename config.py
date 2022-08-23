6cc7124fc641a7c0c0895e471be22987c5c8bf03
Irina White <irina.k.white@gmail.com>
2022-08-23 18:51:43 +0200

    All data is merged into one DF & up_to code added

--- config.py
+++
@@ -1,9 +0,0 @@
-SUBSCRIBE_XPATH='/html/body/div[5]//div/div/div[1]/div/div[2]/div[3]'
-
-COOKIE_XPATH='//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]/span'
-
-ANALYSIS_XPATH='//*[@id="match"]/main/section[1]/div[1]/div/div/a[6]'
-
-HOME_ELO_XPATH='//*[@id="mod_team_analysis"]/div/div/div/table/tbody/tr[2]/td[1]/span'
-            
-AWAY_ELO_XPATH='//*[@id="mod_team_analysis"]/div/div/div/table/tbody/tr[2]/td[3]/span'
